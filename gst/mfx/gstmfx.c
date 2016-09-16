812e8388968819d8ac2da43b053cbce5e219784e
Gopal, Puunithaaraj <puunithaaraj.gopal@intel.com>
2016-09-16 10:32:42 +0800

    Added mfxsinkbin compiling option into CMakefiles.txt and only compile mfxsinkbin if mfxvpp and mfxsink option is turned on.w
    Signed-off-by: Gopal, Puunithaaraj <puunithaaraj.gopal@intel.com>

--- gst/mfx/gstmfx.c
+++ gst/mfx/gstmfx.c
@@ -31,6 +31,9 @@
 #ifdef MFX_SINK
 #include "gstmfxsink.h"
 #endif
+#ifdef MFX_SINK_BIN
+#include "gstmfxsinkbin.h"
+#endif
 #ifdef MFX_H264_ENCODER
 #include "gstmfxenc_h264.h"
 #endif
@@ -63,6 +66,11 @@ plugin_init (GstPlugin * plugin)
   gst_element_register (plugin, "mfxsink", GST_RANK_PRIMARY + 1, GST_TYPE_MFXSINK);
 #endif
 
+#ifdef MFX_SINK_BIN
+  gst_element_register (plugin, "mfxsinkbin",
+      GST_RANK_NONE, GST_TYPE_MFX_SINK_BIN);
+#endif
+
 #ifdef MFX_H264_ENCODER
   gst_element_register (plugin, "mfxh264enc",
       GST_RANK_NONE, GST_TYPE_MFXENC_H264);
@@ -87,6 +95,7 @@ plugin_init (GstPlugin * plugin)
   gst_element_register (plugin, "mfxvc1parse",
       GST_RANK_MARGINAL, GST_MFX_TYPE_VC1_PARSE);
 #endif
+
   return TRUE;
 }
 
