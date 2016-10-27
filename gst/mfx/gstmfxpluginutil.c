56ed5529f0bc40745d94f52f850ba8e24f71807c
Ishmael Visayana Sameen <ishmael.visayana.sameen@intel.com>
2016-10-27 16:59:22 +0800

    Some minor refactoring of the MFX peer query function

--- gst/mfx/gstmfxpluginutil.c
+++ gst/mfx/gstmfxpluginutil.c
@@ -261,11 +261,7 @@ gst_mfx_query_peer_has_raw_caps (GstPad * srcpad)
   GstElement *element = NULL;
   GstCaps *caps = NULL, *templ = NULL;
   gchar *element_name = NULL;
-#if GST_CHECK_VERSION(1,8,0)
-  gboolean has_raw_caps = FALSE;
-#else
   gboolean has_raw_caps = TRUE;
-#endif
 
   while (1) {
     peer_sinkpad = gst_pad_get_peer (srcpad);
@@ -284,6 +280,9 @@ gst_mfx_query_peer_has_raw_caps (GstPad * srcpad)
 
     if (GST_IS_BIN (element) &&
         gst_bin_get_by_name (element, "gluploadelement0")) {
+#if GST_CHECK_VERSION(1,8,0)
+      has_raw_caps = FALSE;
+#endif
       goto cleanup;
     }
     else {
@@ -294,8 +293,12 @@ gst_mfx_query_peer_has_raw_caps (GstPad * srcpad)
           continue;
         }
 
-        if (strncmp (element_name, "gluploadelement", 15) == 0)
+        if (strncmp (element_name, "gluploadelement", 15) == 0) {
+#if GST_CHECK_VERSION(1,8,0)
+          has_raw_caps = FALSE;
+#endif
           goto cleanup;
+        }
 
         if (gst_caps_has_mfx_surface (caps))
           has_raw_caps = FALSE;
