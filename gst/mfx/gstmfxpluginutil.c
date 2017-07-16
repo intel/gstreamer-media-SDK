intel/gstreamer-media-SDK

commit 1f98e197f6b9924f1abb1aebee9cfdf5d10e19a6

Removed un-necessary plugin utility functions and added a few comments on future decoder implementation work

gst/mfx/gstmfxpluginutil.c
@@ -78,98 +78,6 @@ gst_mfx_handle_context_query (GstQuery * query, GstMfxTaskAggregator * task)
   return TRUE;
 }
 
-gboolean
-gst_mfx_append_surface_caps (GstCaps * out_caps, GstCaps * in_caps)
-{
-  GstStructure *structure;
-  const GValue *v_width, *v_height, *v_framerate, *v_par;
-  guint i, n_structures;
-
-  structure = gst_caps_get_structure (in_caps, 0);
-  v_width = gst_structure_get_value (structure, "width");
-  v_height = gst_structure_get_value (structure, "height");
-  v_framerate = gst_structure_get_value (structure, "framerate");
-  v_par = gst_structure_get_value (structure, "pixel-aspect-ratio");
-  if (!v_width || !v_height)
-    return FALSE;
-
-  n_structures = gst_caps_get_size (out_caps);
-  for (i = 0; i < n_structures; i++) {
-    structure = gst_caps_get_structure (out_caps, i);
-    gst_structure_set_value (structure, "width", v_width);
-    gst_structure_set_value (structure, "height", v_height);
-    if (v_framerate)
-      gst_structure_set_value (structure, "framerate", v_framerate);
-    if (v_par)
-      gst_structure_set_value (structure, "pixel-aspect-ratio", v_par);
-  }
-  return TRUE;
-}
-
-gboolean
-gst_mfx_value_set_format (GValue * value, GstVideoFormat format)
-{
-  const gchar *str;
-
-  str = gst_video_format_to_string (format);
-  if (!str)
-    return FALSE;
-
-  g_value_init (value, G_TYPE_STRING);
-  g_value_set_string (value, str);
-  return TRUE;
-}
-
-void
-set_video_template_caps (GstCaps * caps)
-{
-  GstStructure *const structure = gst_caps_get_structure (caps, 0);
-
-  gst_structure_set (structure,
-      "width", GST_TYPE_INT_RANGE, 1, G_MAXINT,
-      "height", GST_TYPE_INT_RANGE, 1, G_MAXINT,
-      "framerate", GST_TYPE_FRACTION_RANGE, 0, 1, G_MAXINT, 1,
-      "pixel-aspect-ratio", GST_TYPE_FRACTION_RANGE, 0, 1, G_MAXINT, 1, NULL);
-}
-
-GstCaps *
-gst_mfx_video_format_new_template_caps (GstVideoFormat format)
-{
-  GstCaps *caps;
-
-  g_return_val_if_fail (format != GST_VIDEO_FORMAT_UNKNOWN, NULL);
-
-  caps = gst_caps_new_empty_simple ("video/x-raw");
-  if (!caps)
-    return NULL;
-
-  gst_caps_set_simple (caps,
-      "format", G_TYPE_STRING, gst_video_format_to_string (format), NULL);
-  set_video_template_caps (caps);
-
-  return caps;
-}
-
-GstCaps *
-gst_mfx_video_format_new_template_caps_with_features (GstVideoFormat format,
-    const gchar * features_string)
-{
-  GstCaps *caps;
-
-  GstCapsFeatures *const features =
-      gst_caps_features_new (features_string, NULL);
-
-  if (!features)
-    return NULL;
-
-  caps = gst_mfx_video_format_new_template_caps (format);
-  if (!caps)
-    return NULL;
-
-  gst_caps_set_features (caps, 0, features);
-  return caps;
-}
-
 GstMfxCapsFeature
 gst_mfx_find_preferred_caps_feature (GstPad * pad,
   gboolean use_10bpc, GstVideoFormat * out_format_ptr)

gst/mfx/gstmfxpluginutil.h
@@ -26,48 +26,14 @@
 #include "gstmfxvideomemory.h"
 
 #include <gst-libs/mfx/gstmfxtaskaggregator.h>
-#include <gst-libs/mfx/gstmfxsurface.h>
 
-gboolean
-gst_mfx_ensure_aggregator(GstElement * element);
-
-gboolean
-gst_mfx_handle_context_query (GstQuery * query, GstMfxTaskAggregator * context);
-
-gboolean
-gst_mfx_append_surface_caps(GstCaps * out_caps, GstCaps * in_caps);
-
-/* Helpers for GValue construction for video formats */
-gboolean
-gst_mfx_value_set_format(GValue * value, GstVideoFormat format);
-
-/* Helpers to build video caps */
+ /* Helpers to build video caps */
 typedef enum
 {
   GST_MFX_CAPS_FEATURE_NOT_NEGOTIATED,
   GST_MFX_CAPS_FEATURE_SYSTEM_MEMORY,
   GST_MFX_CAPS_FEATURE_MFX_SURFACE,
 } GstMfxCapsFeature;
-
-GstCaps *
-gst_mfx_video_format_new_template_caps(GstVideoFormat format);
-
-GstCaps *
-gst_mfx_video_format_new_template_caps_with_features(GstVideoFormat format,
-  const gchar * features_string);
-
-GstMfxCapsFeature
-gst_mfx_find_preferred_caps_feature(GstPad * pad,
-  gboolean use_10bpc, GstVideoFormat * out_format_ptr);
-
-const gchar *
-gst_mfx_caps_feature_to_string(GstMfxCapsFeature feature);
-
-/* Helpers to handle interlaced contents */
-#define GST_CAPS_INTERLACED_MODES \
-    "interlace-mode = (string){ progressive, interleaved, mixed }"
-
-
 
 #ifdef WITH_LIBVA_BACKEND
 # define GST_MFX_MAKE_SURFACE_CAPS        \
@@ -91,6 +57,22 @@ gst_mfx_caps_feature_to_string(GstMfxCapsFeature feature);
     "{ NV12, BGRA, P010_10LE }"
 #endif
 
+/* Helpers to handle interlaced contents */
+#define GST_CAPS_INTERLACED_MODES \
+    "interlace-mode = (string){ progressive, interleaved, mixed }"
+
+gboolean
+gst_mfx_ensure_aggregator(GstElement * element);
+
+gboolean
+gst_mfx_handle_context_query (GstQuery * query, GstMfxTaskAggregator * context);
+
+GstMfxCapsFeature
+gst_mfx_find_preferred_caps_feature(GstPad * pad,
+  gboolean use_10bpc, GstVideoFormat * out_format_ptr);
+
+const gchar *
+gst_mfx_caps_feature_to_string(GstMfxCapsFeature feature);
 
 gboolean
 gst_caps_has_mfx_surface(GstCaps * caps);

gst-libs/mfx/gstmfxdecoder.c
@@ -284,7 +284,9 @@ gst_mfx_decoder_reconfigure_params (GstMfxDecoder * decoder)
   frame_info->FrameRateExtN = decoder->info.fps_n;
   frame_info->FrameRateExtD = decoder->info.fps_d;
 
-  /* We may need to overallocate surfaces when used with decodebin */
+  /* We may need to overallocate surfaces when used with decodebin
+   * TODO: Figure out why jerky playback issues occur with decodebin
+   * and remove this hack */
   if (decoder->is_autoplugged) {
     decoder->params.mfx.CodecLevel = 0;
     decoder->params.mfx.MaxDecFrameBuffering = 0;
@@ -761,6 +763,8 @@ gst_mfx_decoder_decode (GstMfxDecoder * decoder,
   }
 
   if (syncp) {
+    /* TODO: Implement a more robust mechanism to deal with streams that
+     * change properties in the middle of playback */
     if (decoder->num_partial_frames) {
       GstVideoCodecFrame *cur_frame;
       guint n = g_queue_get_length(&decoder->pending_frames) - 1;

commit 51f509ec129e471d6d978c47852f08cf5944f965

Fixed Linux compilation regression caused by commit 1f98e19

gst/mfx/gstmfxpluginutil.c
@@ -78,6 +78,56 @@ gst_mfx_handle_context_query (GstQuery * query, GstMfxTaskAggregator * task)
   return TRUE;
 }
 
+static void
+set_video_template_caps (GstCaps * caps)
+{
+  GstStructure *const structure = gst_caps_get_structure (caps, 0);
+
+  gst_structure_set (structure,
+      "width", GST_TYPE_INT_RANGE, 1, G_MAXINT,
+      "height", GST_TYPE_INT_RANGE, 1, G_MAXINT,
+      "framerate", GST_TYPE_FRACTION_RANGE, 0, 1, G_MAXINT, 1,
+      "pixel-aspect-ratio", GST_TYPE_FRACTION_RANGE, 0, 1, G_MAXINT, 1, NULL);
+}
+
+GstCaps *
+gst_mfx_video_format_new_template_caps (GstVideoFormat format)
+{
+  GstCaps *caps;
+
+  g_return_val_if_fail (format != GST_VIDEO_FORMAT_UNKNOWN, NULL);
+
+  caps = gst_caps_new_empty_simple ("video/x-raw");
+  if (!caps)
+    return NULL;
+
+  gst_caps_set_simple (caps,
+      "format", G_TYPE_STRING, gst_video_format_to_string (format), NULL);
+  set_video_template_caps (caps);
+
+  return caps;
+}
+
+GstCaps *
+gst_mfx_video_format_new_template_caps_with_features (GstVideoFormat format,
+    const gchar * features_string)
+{
+  GstCaps *caps;
+
+  GstCapsFeatures *const features =
+      gst_caps_features_new (features_string, NULL);
+
+  if (!features)
+    return NULL;
+
+  caps = gst_mfx_video_format_new_template_caps (format);
+  if (!caps)
+    return NULL;
+
+  gst_caps_set_features (caps, 0, features);
+  return caps;
+}
+
 GstMfxCapsFeature
 gst_mfx_find_preferred_caps_feature (GstPad * pad,
   gboolean use_10bpc, GstVideoFormat * out_format_ptr)

gst/mfx/gstmfxpluginutil.h
@@ -67,6 +67,13 @@ gst_mfx_ensure_aggregator(GstElement * element);
 gboolean
 gst_mfx_handle_context_query (GstQuery * query, GstMfxTaskAggregator * context);
 
+GstCaps *
+gst_mfx_video_format_new_template_caps(GstVideoFormat format);
+
+GstCaps *
+gst_mfx_video_format_new_template_caps_with_features(GstVideoFormat format,
+  const gchar * features_string);
+
 GstMfxCapsFeature
 gst_mfx_find_preferred_caps_feature(GstPad * pad,
   gboolean use_10bpc, GstVideoFormat * out_format_ptr);