75a9b9cf90e76c139e87c83d9bdc5f775f2ade81
Lim Siew Hoon <siew.hoon.lim@intel.com>
2017-11-30 15:10:22 +0800

    Bump version to 1.3.3-rc5
    Signed-off-by: Lim Siew Hoon <siew.hoon.lim@intel.com>

--- cmake/ProjectInfo.cmake
+++ cmake/ProjectInfo.cmake
@@ -1,8 +1,9 @@
 set(GstMfx_VERSION_MAJOR   1)
 set(GstMfx_VERSION_MINOR   3)
-set(GstMfx_VERSION_PATCH   3)
+set(GstMfx_VERSION_MACRO   3)
+set(GstMfx_VERSION_PATCH   rc5)
 set(GstMfx_PACKAGE         "gstmfx")
 set(GstMfx_PACKAGE_NAME    "GStreamer Media SDK")
 set(GstMfx_PACKAGE_TARNAME "gstreamer-msdk")
 set(GstMfx_VERSION
-  ${GstMfx_VERSION_MAJOR}.${GstMfx_VERSION_MINOR}.${GstMfx_VERSION_PATCH})
+  ${GstMfx_VERSION_MAJOR}.${GstMfx_VERSION_MINOR}.${GstMfx_VERSION_MACRO}.${GstMfx_VERSION_PATCH})
