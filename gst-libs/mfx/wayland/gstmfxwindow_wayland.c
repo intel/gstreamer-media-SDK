intel/gstreamer-media-SDK

commit 4105a40926f0fef01e28c3baca3817cd10fbf7ce

libs: mfx: display: Fixed memory leak from unreleased GstMfxDisplay object on Wayland, and various cleanups/simplifications related to GstMfxDisplay/GstMfxWindowWayland modules.
Fixes #57

Signed-off-by: Ishmael Visayana Sameen <ishmael1985@gmail.com>

gst-libs/mfx/wayland/gstmfxwindow_wayland.c
@@ -45,42 +45,6 @@
 
 
 typedef struct _GstMfxWindowWaylandPrivate GstMfxWindowWaylandPrivate;
-typedef struct _FrameState FrameState;
-
-struct _FrameState
-{
-  GstMfxWindow *window;
-  struct wl_callback *callback;
-  gboolean done;
-};
-
-static FrameState *
-frame_state_new (GstMfxWindow *window)
-{
-  FrameState *frame;
-
-  frame = g_slice_new (FrameState);
-  if (!frame)
-    return NULL;
-
-  frame->window = window;
-  frame->callback = NULL;
-  frame->done = FALSE;
-  return frame;
-}
-
-static void
-frame_state_free (FrameState *frame)
-{
-  if (!frame)
-    return;
-
-  if (frame->callback) {
-    wl_callback_destroy (frame->callback);
-    frame->callback = NULL;
-  }
-  g_slice_free (FrameState, frame);
-}
 
 struct _GstMfxWindowWaylandPrivate
 {
@@ -91,7 +55,7 @@ struct _GstMfxWindowWaylandPrivate
   struct wl_region *opaque_region;
   struct wl_viewport *viewport;
   struct wl_event_queue *event_queue;
-  FrameState *last_frame;
+  struct wl_callback *callback;
   GThread *thread;
   GstPoll *poll;
   GstPollFD pollfd;
@@ -117,20 +81,12 @@ struct _GstMfxWindowWayland
 G_DEFINE_TYPE (GstMfxWindowWayland, gst_mfx_window_wayland,
     GST_TYPE_MFX_WINDOW);
 
-static inline gboolean
-frame_done (FrameState *frame)
-{
-  GstMfxWindowWaylandPrivate *const priv =
-         GST_MFX_WINDOW_WAYLAND_GET_PRIVATE (frame->window);
-  g_atomic_int_set (&frame->done, TRUE);
-  g_atomic_pointer_compare_and_exchange (&priv->last_frame, frame, NULL);
-  return g_atomic_int_dec_and_test (&priv->num_frames_pending);
-}
-
 static void
 frame_done_callback (void *data, struct wl_callback *callback, uint32_t time)
 {
-  frame_done (data);
+  GstMfxWindowWaylandPrivate *const priv =
+      GST_MFX_WINDOW_WAYLAND_GET_PRIVATE (data);
+  g_atomic_int_dec_and_test (&priv->num_frames_pending);
 }
 
 static const struct wl_callback_listener frame_callback_listener = {
@@ -140,11 +96,14 @@ static const struct wl_callback_listener frame_callback_listener = {
 static void
 frame_release_callback (void *data, struct wl_buffer *wl_buffer)
 {
-  FrameState *const frame = data;
-  if (!frame->done)
-    frame_done (frame);
+  GstMfxWindowWaylandPrivate *priv =
+      GST_MFX_WINDOW_WAYLAND_GET_PRIVATE (data);
+
   wl_buffer_destroy (wl_buffer);
-  frame_state_free (frame);
+  if (priv->callback) {
+    wl_callback_destroy (priv->callback);
+    priv->callback = NULL;
+  }
 }
 
 static const struct wl_buffer_listener frame_buffer_listener = {
@@ -231,11 +190,9 @@ gst_mfx_window_wayland_render (GstMfxWindow * window,
   struct wl_display *const display = GST_MFX_DISPLAY_HANDLE (priv->display);
   GstMfxPrimeBufferProxy *buffer_proxy;
   struct wl_buffer *buffer;
-  FrameState *frame;
   guintptr fd = 0;
   guint32 drm_format = 0;
-  gint offsets[3] = { 0 }, pitches[3] = {
-  0}, num_planes = 0, i = 0;
+  gint offsets[3] = { 0 }, pitches[3] = {0}, num_planes = 0, i = 0;
   VaapiImage *vaapi_image;
 
   buffer_proxy = gst_mfx_prime_buffer_proxy_new_from_surface (surface);
@@ -274,19 +231,14 @@ gst_mfx_window_wayland_render (GstMfxWindow * window,
   GST_MFX_DISPLAY_LOCK (priv->display);
   buffer =
       wl_drm_create_prime_buffer (display_priv->drm, fd,
-      src_rect->width, src_rect->height, drm_format,
-      offsets[0], pitches[0], offsets[1], pitches[1], offsets[2], pitches[2]);
+        src_rect->width, src_rect->height, drm_format,
+        offsets[0], pitches[0], offsets[1], pitches[1], offsets[2], pitches[2]);
   GST_MFX_DISPLAY_UNLOCK (priv->display);
   if (!buffer) {
     GST_ERROR ("No wl_buffer created\n");
     goto error;
   }
 
-  frame = frame_state_new (window);
-  if (!frame)
-    goto error;
-
-  g_atomic_pointer_set (&priv->last_frame, frame);
   g_atomic_int_inc (&priv->num_frames_pending);
 
   GST_MFX_DISPLAY_LOCK (priv->display);
@@ -299,10 +251,10 @@ gst_mfx_window_wayland_render (GstMfxWindow * window,
     priv->opaque_region = NULL;
   }
   wl_proxy_set_queue ((struct wl_proxy *) buffer, priv->event_queue);
-  wl_buffer_add_listener (buffer, &frame_buffer_listener, frame);
+  wl_buffer_add_listener (buffer, &frame_buffer_listener, window);
 
-  frame->callback = wl_surface_frame (priv->surface);
-  wl_callback_add_listener (frame->callback, &frame_callback_listener, frame);
+  priv->callback = wl_surface_frame (priv->surface);
+  wl_callback_add_listener (priv->callback, &frame_callback_listener, window);
 
   wl_surface_commit (priv->surface);
   wl_display_flush (display);
@@ -444,7 +396,6 @@ gst_mfx_window_wayland_destroy (GstMfxWindow * window)
 {
   GstMfxWindowWaylandPrivate *const priv =
       GST_MFX_WINDOW_WAYLAND_GET_PRIVATE (window);
-
   struct wl_display *const display =
       GST_MFX_DISPLAY_HANDLE (priv->display);
 
@@ -477,6 +428,7 @@ gst_mfx_window_wayland_destroy (GstMfxWindow * window)
     wl_shell_surface_destroy (priv->shell_surface);
     priv->shell_surface = NULL;
   }
+
   if (priv->surface) {
     wl_surface_destroy (priv->surface);
     priv->surface = NULL;
@@ -486,7 +438,9 @@ gst_mfx_window_wayland_destroy (GstMfxWindow * window)
     wl_event_queue_destroy (priv->event_queue);
     priv->event_queue = NULL;
   }
+
   gst_poll_free (priv->poll);
+  gst_mfx_display_unref (priv->display);
 }
 
 static gboolean

gst-libs/mfx/gstmfxdisplay.c
@@ -173,7 +173,7 @@ static void
 gst_mfx_display_destroy (GstMfxDisplay * display)
 {
   GstMfxDisplayPrivate *const priv = GST_MFX_DISPLAY_GET_PRIVATE (display);
-  GstMfxDisplayClass *klass = GST_MFX_DISPLAY_GET_CLASS (display);
+  const GstMfxDisplayClass *const klass = GST_MFX_DISPLAY_GET_CLASS (display);
 
   if (priv->va_display) {
     vaTerminate (priv->va_display);
@@ -183,6 +183,7 @@ gst_mfx_display_destroy (GstMfxDisplay * display)
     close (priv->display_fd);
     priv->display_fd = 0;
   }
+
   if (klass->close_display)
     klass->close_display (display);
 }
@@ -261,8 +262,6 @@ gst_mfx_display_init (GstMfxDisplay * display)
 {
   GstMfxDisplayPrivate *const priv =
       gst_mfx_display_get_instance_private (display);
-  const GstMfxDisplayClass *const dpy_class =
-      GST_MFX_DISPLAY_GET_CLASS (display);
 
   priv->display_type = GST_MFX_DISPLAY_TYPE_ANY;
   priv->par_n = 1;
@@ -271,9 +270,6 @@ gst_mfx_display_init (GstMfxDisplay * display)
   g_rec_mutex_init (&priv->mutex);
 
   display->priv = priv;
-
-  if (dpy_class->init)
-    dpy_class->init (display);
 }
 
 static void

gst-libs/mfx/gstmfxdisplay_priv.h
@@ -24,8 +24,6 @@
 #define GST_MFX_DISPLAY_PRIV_H
 
 #include "gstmfxdisplay.h"
-#include "gstmfxwindow.h"
-#include "gstmfxwindow_priv.h"
 
 G_BEGIN_DECLS
 #define GST_MFX_DISPLAY_CAST(display) \
@@ -33,13 +31,12 @@ G_BEGIN_DECLS
 #define GST_MFX_DISPLAY_GET_PRIVATE(display) \
   (GST_MFX_DISPLAY (display)->priv)
 #define GST_MFX_DISPLAY_CLASS(klass) \
-  ((GstMfxDisplayClass *) (klass))
+  ((GstMfxDisplayClass *)(klass))
 #define GST_MFX_DISPLAY_GET_CLASS(obj) \
-  GST_MFX_DISPLAY_CLASS (GST_OBJECT_GET_CLASS (obj))
+  GST_MFX_DISPLAY_CLASS(GST_OBJECT_GET_CLASS (obj))
 typedef struct _GstMfxDisplayPrivate GstMfxDisplayPrivate;
 typedef struct _GstMfxDisplayClass GstMfxDisplayClass;
 
-typedef void (*GstMfxDisplayInitFunc) (GstMfxDisplay * display);
 typedef gboolean (*GstMfxDisplayOpenFunc) (GstMfxDisplay * display,
     const gchar * name);
 typedef void (*GstMfxDisplayCloseFunc) (GstMfxDisplay * display);
@@ -118,7 +115,6 @@ struct _GstMfxDisplayClass
   guint display_type;
 
   /*< public > */
-  GstMfxDisplayInitFunc init;
   GstMfxDisplayOpenFunc open_display;
   GstMfxDisplayCloseFunc close_display;
   GstMfxDisplayGetSizeFunc get_size;

gst-libs/mfx/wayland/gstmfxdisplay_wayland.c
@@ -27,7 +27,6 @@
 #include <fcntl.h>
 #include <xf86drm.h>
 #include <intel_bufmgr.h>
-#include "gstmfxdisplay_priv.h"
 #include "gstmfxdisplay_wayland.h"
 #include "gstmfxdisplay_wayland_priv.h"
 #include "gstmfxwindow_wayland.h"
@@ -54,8 +53,6 @@ set_display_name (GstMfxDisplay * display, const gchar * display_name)
 {
   GstMfxDisplayWaylandPrivate *const priv =
       GST_MFX_DISPLAY_WAYLAND_GET_PRIVATE (display);
-
-  //g_free (priv->display_name);
 
   if (!display_name) {
     display_name = get_default_display_name ();
@@ -305,21 +302,20 @@ gst_mfx_display_wayland_get_size_mm (GstMfxDisplay * display,
 }
 
 static void
-gst_mfx_display_wayland_init (GstMfxDisplayWayland * display)
-{
-}
-
-static void
 gst_mfx_display_wayland_class_init (GstMfxDisplayWaylandClass * klass)
 {
   GstMfxDisplayClass *const dpy_class = GST_MFX_DISPLAY_CLASS (klass);
 
   dpy_class->display_type = GST_MFX_DISPLAY_TYPE_WAYLAND;
-
   dpy_class->open_display = gst_mfx_display_wayland_open_display;
   dpy_class->close_display = gst_mfx_display_wayland_close_display;
   dpy_class->get_size = gst_mfx_display_wayland_get_size;
   dpy_class->get_size_mm = gst_mfx_display_wayland_get_size_mm;
+}
+
+static void
+gst_mfx_display_wayland_init (GstMfxDisplayWayland * display)
+{
 }
 
 /**

gst-libs/mfx/x11/gstmfxdisplay_x11.c
@@ -22,7 +22,6 @@
 
 #include "sysdeps.h"
 
-#include "gstmfxdisplay_priv.h"
 #include "gstmfxdisplay_x11.h"
 #include "gstmfxdisplay_x11_priv.h"
 #include "gstmfxwindow_x11.h"
@@ -180,7 +179,7 @@ gst_mfx_display_x11_get_size_mm (GstMfxDisplay * display,
     *pheight = height_mm;
 }
 
-void
+static void
 gst_mfx_display_x11_class_init (GstMfxDisplayX11Class * klass)
 {
   GstMfxDisplayClass *const dpy_class = GST_MFX_DISPLAY_CLASS (klass);
@@ -208,8 +207,14 @@ gst_mfx_display_x11_init (GstMfxDisplayX11 * display)
  * Return value: a newly allocated #GstMfxDisplay object
  */
 GstMfxDisplay *
-gst_mfx_display_x11_new (GstMfxDisplayX11 * display, const gchar * display_name)
+gst_mfx_display_x11_new (const gchar * display_name)
 {
+  GstMfxDisplayX11 *display;
+
+  display = g_object_new (GST_TYPE_MFX_DISPLAY_X11, NULL);
+  if (!display)
+    return NULL;
+
   return gst_mfx_display_new_internal (GST_MFX_DISPLAY (display),
       (gpointer) display_name);
 }

gst-libs/mfx/x11/gstmfxdisplay_x11.h
@@ -36,8 +36,7 @@ G_DECLARE_FINAL_TYPE (GstMfxDisplayX11, gst_mfx_display_x11, GST_MFX,
   GstMfxDisplayX11Class))
 
 GstMfxDisplay *
-gst_mfx_display_x11_new (GstMfxDisplayX11 * display,
-    const gchar * display_name);
+gst_mfx_display_x11_new (const gchar * display_name);
 
 Display *gst_mfx_display_x11_get_display (GstMfxDisplay * display);
 

gst/mfx/gstmfxsink.c
@@ -637,9 +637,7 @@ gst_mfxsink_set_render_backend (GstMfxSink * sink)
   switch (sink->display_type_req) {
 #ifdef USE_DRI3
     case GST_MFX_DISPLAY_TYPE_X11:
-      display =
-          gst_mfx_display_x11_new (g_object_new (GST_TYPE_MFX_DISPLAY_X11,
-              NULL), sink->display_name);
+      display = gst_mfx_display_x11_new (sink->display_name);
       if (!display)
         goto display_unsupported;
       sink->backend = gst_mfxsink_backend_x11 ();
@@ -814,6 +812,7 @@ gst_mfxsink_stop (GstBaseSink * base_sink)
   if (!sink->foreign_window) {
 #ifdef WITH_LIBVA_BACKEND
     gst_mfxsink_set_event_handling (sink, FALSE);
+    gst_mfx_display_replace (&sink->display, NULL);
 #endif
     gst_mfx_window_replace (&sink->window, NULL);
   }

commit 11c9633dd56f58eedfecd45fea372d971f7c2df8

libs:window:wayland: Re-instate commit de72453 which fixes memory leak regression introduced with commit 4105a40.

gst-libs/mfx/wayland/gstmfxwindow_wayland.c
@@ -45,6 +45,42 @@
 
 
 typedef struct _GstMfxWindowWaylandPrivate GstMfxWindowWaylandPrivate;
+typedef struct _FrameState FrameState;
+
+struct _FrameState
+{
+  GstMfxWindow *window;
+  struct wl_callback *callback;
+  gboolean done;
+};
+
+static FrameState *
+frame_state_new (GstMfxWindow * window)
+{
+  FrameState *frame;
+
+  frame = g_slice_new (FrameState);
+  if (!frame)
+    return NULL;
+
+  frame->window = window;
+  frame->callback = NULL;
+  frame->done = FALSE;
+  return frame;
+}
+
+static void
+frame_state_free (FrameState * frame)
+{
+  if (!frame)
+    return;
+
+  if (frame->callback) {
+    wl_callback_destroy (frame->callback);
+    frame->callback = NULL;
+  }
+  g_slice_free (FrameState, frame);
+}
 
 struct _GstMfxWindowWaylandPrivate
 {
@@ -55,7 +91,7 @@ struct _GstMfxWindowWaylandPrivate
   struct wl_region *opaque_region;
   struct wl_viewport *viewport;
   struct wl_event_queue *event_queue;
-  struct wl_callback *callback;
+  FrameState *last_frame;
   GThread *thread;
   GstPoll *poll;
   GstPollFD pollfd;
@@ -81,12 +117,20 @@ struct _GstMfxWindowWayland
 G_DEFINE_TYPE (GstMfxWindowWayland, gst_mfx_window_wayland,
     GST_TYPE_MFX_WINDOW);
 
-static void
-frame_done_callback (void *data, struct wl_callback *callback, uint32_t time)
+static inline gboolean
+frame_done (FrameState * frame)
 {
   GstMfxWindowWaylandPrivate *const priv =
-      GST_MFX_WINDOW_WAYLAND_GET_PRIVATE (data);
-  g_atomic_int_dec_and_test (&priv->num_frames_pending);
+      GST_MFX_WINDOW_WAYLAND_GET_PRIVATE (frame->window);
+  g_atomic_int_set (&frame->done, TRUE);
+  g_atomic_pointer_compare_and_exchange (&priv->last_frame, frame, NULL);
+  return g_atomic_int_dec_and_test (&priv->num_frames_pending);
+}
+
+static void
+frame_done_callback (void * data, struct wl_callback * callback, uint32_t time)
+{
+  frame_done (data);
 }
 
 static const struct wl_callback_listener frame_callback_listener = {
@@ -94,21 +138,19 @@ static const struct wl_callback_listener frame_callback_listener = {
 };
 
 static void
-frame_release_callback (void *data, struct wl_buffer *wl_buffer)
+frame_release_callback (void * data, struct wl_buffer * wl_buffer)
 {
-  GstMfxWindowWaylandPrivate *priv =
-      GST_MFX_WINDOW_WAYLAND_GET_PRIVATE (data);
-
+  FrameState *const frame = data;
+  if (!frame->done)
+    frame_done (frame);
   wl_buffer_destroy (wl_buffer);
-  if (priv->callback) {
-    wl_callback_destroy (priv->callback);
-    priv->callback = NULL;
-  }
+  frame_state_free (frame);
 }
 
 static const struct wl_buffer_listener frame_buffer_listener = {
   frame_release_callback
 };
+
 
 /**
  * GstMfxWindowWaylandClass:
@@ -190,6 +232,7 @@ gst_mfx_window_wayland_render (GstMfxWindow * window,
   struct wl_display *const display = GST_MFX_DISPLAY_HANDLE (priv->display);
   GstMfxPrimeBufferProxy *buffer_proxy;
   struct wl_buffer *buffer;
+  FrameState *frame;
   guintptr fd = 0;
   guint32 drm_format = 0;
   gint offsets[3] = { 0 }, pitches[3] = {0}, num_planes = 0, i = 0;
@@ -239,6 +282,11 @@ gst_mfx_window_wayland_render (GstMfxWindow * window,
     goto error;
   }
 
+  frame = frame_state_new (window);
+  if (!frame)
+    goto error;
+
+  g_atomic_pointer_set (&priv->last_frame, frame);
   g_atomic_int_inc (&priv->num_frames_pending);
 
   GST_MFX_DISPLAY_LOCK (priv->display);
@@ -251,10 +299,10 @@ gst_mfx_window_wayland_render (GstMfxWindow * window,
     priv->opaque_region = NULL;
   }
   wl_proxy_set_queue ((struct wl_proxy *) buffer, priv->event_queue);
-  wl_buffer_add_listener (buffer, &frame_buffer_listener, window);
+  wl_buffer_add_listener (buffer, &frame_buffer_listener, frame);
 
-  priv->callback = wl_surface_frame (priv->surface);
-  wl_callback_add_listener (priv->callback, &frame_callback_listener, window);
+  frame->callback = wl_surface_frame (priv->surface);
+  wl_callback_add_listener (frame->callback, &frame_callback_listener, frame);
 
   wl_surface_commit (priv->surface);
   wl_display_flush (display);
@@ -282,20 +330,20 @@ gst_mfx_window_wayland_hide (GstMfxWindow * window)
 }
 
 static void
-handle_ping (void *data, struct wl_shell_surface *shell_surface,
+handle_ping (void * data, struct wl_shell_surface * shell_surface,
     uint32_t serial)
 {
   wl_shell_surface_pong (shell_surface, serial);
 }
 
 static void
-handle_configure (void *data, struct wl_shell_surface *shell_surface,
+handle_configure (void * data, struct wl_shell_surface * shell_surface,
     uint32_t edges, int32_t width, int32_t height)
 {
 }
 
 static void
-handle_popup_done (void *data, struct wl_shell_surface *shell_surface)
+handle_popup_done (void * data, struct wl_shell_surface * shell_surface)
 {
 }
 