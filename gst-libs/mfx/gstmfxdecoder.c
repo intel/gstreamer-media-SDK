intel/gstreamer-media-SDK

commit bffa5e9a0b053b1ed6d96670e60439cf72129eb7

mfxjpegdec: fix decode issues with JPEG444 formats
Fixes intel/gstreamer-media-SDK#110.

gst-libs/mfx/gstmfxdecoder.c
@@ -271,6 +271,11 @@ gst_mfx_decoder_reconfigure_params (GstMfxDecoder * decoder)
     frame_info->Height = GST_ROUND_UP_32 (decoder->info.height);
   }
 
+  if (decoder->profile.codec == MFX_CODEC_JPEG) {
+    frame_info->FourCC = MFX_FOURCC_RGB4;
+    frame_info->ChromaFormat = MFX_CHROMAFORMAT_YUV444;
+  }
+
   frame_info->FrameRateExtN = decoder->info.fps_n;
   frame_info->FrameRateExtD = decoder->info.fps_d;
 

commit caa5ce6ad56a2672b5db5d3249e881325e85470b

mfxjpegdec: fix JPEG decode regression on non-APL platforms caused by commit bffa5e9

gst-libs/mfx/gstmfxdecoder.c
@@ -271,7 +271,8 @@ gst_mfx_decoder_reconfigure_params (GstMfxDecoder * decoder)
     frame_info->Height = GST_ROUND_UP_32 (decoder->info.height);
   }
 
-  if (decoder->profile.codec == MFX_CODEC_JPEG) {
+  if (decoder->profile.codec == MFX_CODEC_JPEG
+      && decoder->params.mfx.JPEGChromaFormat == MFX_CHROMAFORMAT_YUV444) {
     frame_info->FourCC = MFX_FOURCC_RGB4;
     frame_info->ChromaFormat = MFX_CHROMAFORMAT_YUV444;
   }